539a0812aff9600cd91071167d412db7c709284a
Michael Bunsen <notbot@gmail.com>
2017-02-11 13:48:29 -0800

    Add basic support for rendering an SVG template

--- footer/magic/tests.py
+++ footer/magic/tests.py
@@ -1,3 +1,33 @@
+from __future__ import print_function
+import tempfile
 from django.test import TestCase
 
+
+import cairosvg
+
 # Create your tests here.
+
+class ImageTests(TestCase):
+
+    def test_make_svg(self):
+        from .images import make_svg
+        context = {'name': 'test'}
+        svg = make_svg(context)
+        self.assertTrue( svg.startswith('<?xml') )
+        self.assertTrue( svg.endswith('</svg>') )
+        for v in context.values():
+            self.assertIn(v, svg)
+
+    def test_write_svg(self):
+        from .images import make_svg, write_svg_to_png
+        context = {'name': 'test'}
+        svg = make_svg(context)
+        #f, fname = tempfile.mkstemp(suffix='.svg') @TODO
+        fname = '/tmp/test.svg'
+        f = open(fname, 'w')
+        f = write_svg_to_png(svg, f)
+        f.close()
+
+        # Check if file is binary
+        file_content = open(fname, 'rb').read()
+        self.assertIn(b'\x00', file_content)
