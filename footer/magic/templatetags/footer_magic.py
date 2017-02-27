beec3678cd819a564855749d03e33f876e855f6f
Michael Bunsen <notbot@gmail.com>
2017-02-27 14:32:07 -0800

    Cleanup unused id/uuid variables

--- footer/magic/templatetags/footer_magic.py
+++ footer/magic/templatetags/footer_magic.py
@@ -8,27 +8,22 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True, name='ii')
 def inline_image(context, text):
-    uuid = context.get('uuid', 'id')
     request = context['request']
-    # view = context['view']
-    #text = "%s-%s" % (text, uuid)
     src = request.build_absolute_uri(reverse('inline_text_image'))
-    src = '%s?param=%s&id=%s' % (src, text, uuid)
+    src = '%s?param=%s&id=%s' % (src, text)
     tag = '<img src="%s" alt="%s" title="%s">' % (src, text, text)
     return mark_safe(tag)
 
 @register.simple_tag(takes_context=True, name='ia')
 def inline_animation(context, text):
-    uuid = context.get('uuid', 'id')
     request = context['request']
     src = request.build_absolute_uri(reverse('inline_text_animation'))
-    src = '%s?param=%s&id=%s' % (src, text, uuid)
+    src = '%s?param=%s' % (src, text)
     tag = '<img src="%s" alt="%s" title="%s">' % (src, text, text)
     return mark_safe(tag)
 
 @register.simple_tag(takes_context=True, name='leader')
 def leader_image(context, position):
-    uuid = context.get('uuid', 'id')
     request = context['request']
     position = position or 'orphan'
     src = request.build_absolute_uri(reverse('leader_image'))
