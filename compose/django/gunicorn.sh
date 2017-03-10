6dea2b808af4a7c19377f81c305432aa5a4c4ce9
Michael Bunsen <notbot@gmail.com>
2017-03-10 14:36:37 -0800

    Install New Relic APM agent for app monitoring

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+NEW_RELIC_CONFIG_FILE=/newrelic.ini
+export NEW_RELIC_CONFIG_FILE
+
 python /app/manage.py collectstatic --noinput
-/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
+newrelic-admin run-program /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
